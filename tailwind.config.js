2338cb9943951817465b2acac247a8fc9e02121b
AkshayDaharwal <akshay30.ad@gmail.com>
2026-05-11 12:27:04 +0530

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,10 +16,10 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        mokoto: ["mokoto", "sans-serif"],
-        montserrat: ["montserrat", "sans-serif"],
-        oxanium: ["oxanium", "sans-serif"],
-        inter: ["inter", "sans-serif"],
+        mokoto: ["Mokoto", "sans-serif"],
+        // montserrat: ["Montserrat", "sans-serif"],
+        // oxanium: ["Oxanium", "sans-serif"],
+        // inter: ["Inter", "sans-serif"],
       },
 
       animation: {
